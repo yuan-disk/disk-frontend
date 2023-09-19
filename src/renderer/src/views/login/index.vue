5e0af0d47da894c3e39f605f94bb0a8bc15aafac
unknown <2068806867@qq.com>
2023-09-19 22:43:12 +0800

    login

--- src/renderer/src/views/login/index.vue
+++ src/renderer/src/views/login/index.vue
@@ -1,99 +1,87 @@
 <template>
-  <el-containner>
-    <el-form
-      ref="ruleFormRef"
-      :model="ruleForm"
-      status-icon
-      :rules="rules"
-      label-width="120px"
-      class="demo-ruleForm"
-    >
-      <el-form-item label="Login" prop="checkPass">
-        <el-input v-model="ruleForm.user" type="password" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="Password" prop="pass">
-        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
-      </el-form-item>
-
-      <el-form-item>
-        <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
-        <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
-      </el-form-item>
-    </el-form>
-  </el-containner>
+  <div class="container">
+    <div class="center">
+      <div class="project-name">原盘</div>
+      <el-form :model="loginForm" :rules="rules" label-width="80px" label-position="">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model.trim="loginForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model.trim="loginForm.password" type="password" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">原盘！启动！</el-button>
+          <router-link to="/registry">没有账号？转到注册</router-link>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import type { FormInstance, FormRules } from 'element-plus'
+<script setup>
+import { ref } from 'vue'
 
-const ruleFormRef = ref<FormInstance>()
-
-const checkAge = (rule: any, value: any, callback: any) => {
-  if (!value) {
-    return callback(new Error('Please input the age'))
-  }
-  setTimeout(() => {
-    if (!Number.isInteger(value)) {
-      callback(new Error('Please input digits'))
-    } else {
-      if (value < 18) {
-        callback(new Error('Age must be greater than 18'))
-      } else {
-        callback()
-      }
-    }
-  }, 1000)
-}
-
-const validatePass = (rule: any, value: any, callback: any) => {
-  if (value === '') {
-    callback(new Error('Please input the password'))
-  } else {
-    if (ruleForm.checkPass !== '') {
-      if (!ruleFormRef.value) return
-      ruleFormRef.value.validateField('checkPass', () => null)
-    }
-    callback()
-  }
-}
-const validatePass2 = (rule: any, value: any, callback: any) => {
-  if (value === '') {
-    callback(new Error('Please input the password again'))
-  } else if (value !== ruleForm.pass) {
-    callback(new Error("Two inputs don't match!"))
-  } else {
-    callback()
-  }
-}
-
-const ruleForm = reactive({
-  pass: '',
-  checkPass: '',
-  age: '',
-  user: ''
+const loginForm = ref({
+  username: '',
+  password: ''
 })
 
-const rules = reactive<FormRules<typeof ruleForm>>({
-  pass: [{ validator: validatePass, trigger: 'blur' }],
-  checkPass: [{ validator: validatePass2, trigger: 'blur' }],
-  age: [{ validator: checkAge, trigger: 'blur' }]
+const rules = ref({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { pattern: /^[a-zA-Z0-9]*$/, message: '用户名只能包含数字和英文字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 5, message: '密码长度必须大于4位', trigger: 'blur' }
+  ]
 })
 
-const submitForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.validate((valid) => {
-    if (valid) {
-      console.log('submit!')
-    } else {
-      console.log('error submit!')
-      return false
-    }
-  })
-}
-
-const resetForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.resetFields()
+const submitForm = () => {
+  const valid = loginForm.value.$refs.validate()
+  if (valid) {
+    alert('提交成功!')
+  } else {
+    console.log('error submit!!')
+    return false
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  width: 100%;
+}
+
+.project-name {
+  font-family: 宋体;
+  text-align: center;
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.center {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+
+.el-form-item {
+  margin-bottom: 20px;
+}
+
+.el-button {
+  width: 230px;
+}
+
+.el-input {
+  width: 230px;
+}
+</style>
--- src/renderer/src/views/registry/index.vue
+++ src/renderer/src/views/registry/index.vue
@@ -1,4 +1,108 @@
 <template>
-    <p>zhucheshouye</p>
-    <router-link to="/login">已有帐号？去登录</router-link>
+  <div class="container">
+    <div class="center">
+      <div class="project-name">原盘</div>
+      <el-form :model="registerForm" :rules="rules" label-width="80px" label-position="">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model.trim="registerForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model.trim="registerForm.password" type="password" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmPassword">
+          <el-input
+            v-model.trim="registerForm.confirmPassword"
+            type="password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">原盘！注册！</el-button>
+          <router-link to="/login">已有账号？转到登录</router-link>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const registerForm = ref({
+  username: '',
+  password: '',
+  confirmPassword: ''
+})
+
+const rules = ref({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { pattern: /^[a-zA-Z0-9]*$/, message: '用户名只能包含数字和英文字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 5, message: '密码长度必须大于4位', trigger: 'blur' }
+  ],
+  confirmPassword: [
+    { required: true, message: '请再次输入密码', trigger: 'blur' },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== registerForm.value.password) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+})
+
+const submitForm = () => {
+  const valid = registerForm.value.$refs.validate()
+  if (valid) {
+    alert('提交成功!')
+  } else {
+    console.log('error submit!!')
+    return false
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  width: 100%;
+}
+
+.project-name {
+  font-family: 宋体;
+  text-align: center;
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.center {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+
+.el-form-item {
+  margin-bottom: 20px;
+}
+
+.el-button {
+  width: 230px;
+}
+
+.el-input {
+  width: 230px;
+}
+</style>
